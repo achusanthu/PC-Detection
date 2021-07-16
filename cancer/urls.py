1036ac232d958cf2aa83fb477776396754b07b47
achusanthu <achusanthu0807@gmail.com>
2021-07-16 12:33:33 +0530

    Initial commit

---
+++ cancer/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+urlpatterns=[
+	path('',views.index,name='index'),
+	path('about',views.about,name='about'),
+	path('contact',views.contact,name='contact'),
+	path('register',views.register,name='register'),
+	path('signin',views.log_in,name='signin'),
+	path('userpredict',views.userpredict,name='userpredict'),
+	path('result',views.result,name='result')
+
+
+]
